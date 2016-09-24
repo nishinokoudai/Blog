2f710a7611ae93e6ac57971fae1086db4863e6fa
nishinokoudai <beat.nishino@gmail.com>
2016-09-25 00:28:38 +0900

    indexの変更！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,20 @@ class CommentsController < ApplicationController
     
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment])
+        @comment = @post.comments.create(comment_params)
        if @comment.save
        	redirect_to post_path(@post)
        else
         render :template => "posts/show"
        end 
+   end
+
+   def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+
+      redirect_to post_path(@post)
    end
 
    private
